93fe14dc5da4ee3b8be7ff21ff306dab19d05bbf
philipsales <philip@alliedworld.healthcare>
2019-08-27 23:58:54 +0800

    Clean folder directory

--- src/domains/messages/typeDefs/query.gql.js
+++ src/domains/messages/typeDefs/query.gql.js
@@ -3,21 +3,7 @@ const { gql } = require('apollo-server');
 const typeDefs = gql`
 
   type Query {
-    launch(id: ID!): Launch
     messages: [Message] 
-  }
-
-  type Launch {
-    id: ID!
-    site: String
-    rocket: Rocket
-    isBooked: Boolean!
-  }
-
-  type Rocket {
-    id: ID!
-    name: String
-    type: String
   }
 
   type Message {
